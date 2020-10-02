b0d94a1515993a33eeeb9ad12346a1298b3f9c5e
Wraithname <43811054+Wraithname@users.noreply.github.com>
2020-10-03 02:03:55 +0300

    Обработка 360 изображений. Путь указывается в коде программы

--- Magistr/Form1.cs
+++ Magistr/Form1.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Security.Cryptography;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Magistr
@@ -13,22 +13,41 @@ namespace Magistr
         List<double> res;
         double angle;
         Bitmap image;
+        string rpath = "C:\\3";
+        string sipath = "C:\\r";
+        int ri = 1;
+        StreamWriter sw;
+        Stopwatch stopwatch = new Stopwatch();
         public Form1()
         {
             res = new List<double>();
+            sw = new StreamWriter(sipath + "\\result.txt");
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            stopwatch.Start();
             button1.Enabled = false;
-            image = new Bitmap(pictureBox1.Image);
-            matr = new Moment(image);
-            matr.GetRes();
-            res = matr.cResult;
-            angle = matr.gradus;
-            showMatrix();
+            for (int i = 1; i < 360; i++)
+            {
+                pictureBox1.Image = Image.FromFile(rpath + "\\" + i + ".png");
+                image = new Bitmap(pictureBox1.Image);
+                matr = new Moment(image);
+                matr.GetRes();
+                res = matr.cResult;
+                angle = matr.gradus;
+                WriteToFile();
+            }
+            stopwatch.Stop();
+            sw.WriteLine("Время обработки: "+ (stopwatch.ElapsedMilliseconds/1000)+" сек");
+            sw.WriteLine("Среднее время на обработку 1 изображения: "+ ((stopwatch.ElapsedMilliseconds / 1000)/360) + " сек");
+            sw.Flush();
+            sw.Close();
+            Application.Exit();
+            //showMatrix();
         }
+        /*
         private void showMatrix()
         {
             txt1.Text = matr.center[0].ToString()+" : "+ matr.center[1].ToString();
@@ -37,6 +56,29 @@ namespace Magistr
             txt6.Text = res[2].ToString();
             textBox1.Text = matr.gradus.ToString();
         }
+        */
+        private void WriteToFile()
+        {
+            sw.WriteLine("Результат обработки изображения " + ri + ".png");
+                sw.WriteLine("Координаты центра: "+ matr.center[0].ToString() + " : " + matr.center[1].ToString());
+                sw.WriteLine("M 1:1 = " + res[0].ToString());
+                sw.WriteLine("M 2:0 = " + res[1].ToString());
+                sw.WriteLine("M 0:2 = " + res[2].ToString());
+                sw.WriteLine("Угол поворота: " + matr.gradus.ToString());
+            using (Bitmap clone = (Bitmap)pictureBox1.Image.Clone())
+            {
+                using (Graphics gfx = Graphics.FromImage(clone))
+                {
+                    gfx.Clear(Color.Transparent);
+                    gfx.TranslateTransform(pictureBox1.Image.Width / 2, pictureBox1.Image.Height / 2);
+                    gfx.RotateTransform((float)angle);
+                    gfx.DrawImage(pictureBox1.Image, -pictureBox1.Image.Width / 2, -pictureBox1.Image.Height / 2);
+                }
+                clone.Save(sipath + "\\" + ri + ".png");
+                ri++;
+            }
+        }
+        /*
         protected override void OnPaint(PaintEventArgs e)
         {
             using (Bitmap clone = (Bitmap)pictureBox1.Image.Clone())
@@ -51,5 +93,6 @@ namespace Magistr
                 pictureBox1.Image = (Bitmap)clone.Clone();
             }
         }
+        */
     }
 }
